1322f84b1f32ba1506d3d8166f353fc48a79676f
JordiSanchezMarin <jordisanchez00@gmail.com>
2014-02-28 13:44:37 +0100

    video casi finalizado
    falta:
    - barra de control de volumen
    - barra de tiempo de video
    - botón mute
    - arreglar el puto footer en blanco

--- Activitat 2.2/Snowboard.js
+++ Activitat 2.2/Snowboard.js
@@ -1,12 +1,12 @@
 var respuesta1 = "";
-var respuesta2 = ""; 7
+var respuesta2 = ""; 
 var respuesta3 = "";
 var video;
-video = document.getElementById("vid");
+//video = document.getElementById("vid");
 pregunta1 = true;
 pregunta2 = true;
 pregunta3 = true;
-            
+respuestas = true;
 function cargar()
 {
     $("#btn_play").click(function()
@@ -69,122 +69,168 @@ function cargar()
             }
         });
     }
-                    if (video.currentTime > 37.5 && pregunta2 == true) {
-                        video.pause();
-                        bootbox.dialog({
-                            message: "a) Girando el tronco superior. <br /> b) Inclinando el cuerpo hacia delante. <br /> c) Inclinando el cuerpo hacia atrás.",
-                            title: "¿Cómo girarías?",
-                            closeButton: false,
-                            animate: true,
-                            buttons: {
-                                success: {
-                                    label: "a",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta2 = "a";
-                                        pregunta2 = false;
-                                        rePlay();
-                                    }
-                                },
-                                danger: {
-                                    label: "b",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta2 = "b";
-                                        pregunta2 = false;
-                                        rePlay();
-                                    }
-                                },
-                                main: {
-                                    label: "c",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta2 = "c";
-                                        pregunta2 = false;
-                                        rePlay();
-                                    }
-                                }
-
-                            }
-                        });
+    if (video.currentTime > 37.5 && pregunta2 == true) {
+        video.pause();
+        bootbox.dialog({
+            message: "a) Girando el tronco superior. <br /> b) Inclinando el cuerpo hacia delante. <br /> c) Inclinando el cuerpo hacia atrás.",
+            title: "¿Cómo girarías?",
+            closeButton: false,
+            animate: true,
+            buttons: {
+                success: {
+                    label: "a",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta2 = "a";
+                        pregunta2 = false;
+                        rePlay();
                     }
-                    if (video.currentTime > 50.5 && pregunta3 == true) {
-                        video.pause();
-                        bootbox.dialog({
-                            message: "a) Tirándome al suelo. <br /> b) Clavando el lateral trasero de la tabla. <br /> c) Clavando el lateral delantero de la tabla.",
-                            title: "¿Como frenarías?",
-                            closeButton: false,
-                            animate: true,
-                            buttons: {
-                                success: {
-                                    label: "a",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta3 = "a";
-                                        pregunta3 = false;
-                                        rePlay();
-                                    }
-                                },
-                                danger: {
-                                    label: "b",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta3 = "b";
-                                        pregunta3 = false;
-                                        rePlay();
-                                    }
-                                },
-                                main: {
-                                    label: "c",
-                                    className: "btn-primary",
-                                    callback: function () {
-                                        respuesta3 = "c";
-                                        pregunta3 = false;
-                                        rePlay();
-                                    }
-                                }
-
-                            }
-                        });
+                },
+                danger: {
+                    label: "b",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta2 = "b";
+                        pregunta2 = false;
+                        rePlay();
                     }
-                });
-            }
-
-            function rePlay() {
-                setTimeout(function () {
-                    video.play();
-                }, 1000);
-            }
-
-            function volumeDown() {
-                video.volume = video.volume - 0.1;
-            }
-
-            function volumeUp() {
-                video.volume = video.volume + 0.1;
-            }
-
-            function fullScreen() {
-                if (!document.fullscreenElement &&    // alternative standard method
-      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
-                    if (document.documentElement.requestFullscreen) {
-                        document.documentElement.requestFullscreen();
-                    } else if (document.documentElement.msRequestFullscreen) {
-                        document.documentElement.msRequestFullscreen();
-                    } else if (document.documentElement.mozRequestFullScreen) {
-                        document.documentElement.mozRequestFullScreen();
-                    } else if (document.documentElement.webkitRequestFullscreen) {
-                        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-                    }
-                } else {
-                    if (document.exitFullscreen) {
-                        document.exitFullscreen();
-                    } else if (document.msExitFullscreen) {
-                        document.msExitFullscreen();
-                    } else if (document.mozCancelFullScreen) {
-                        document.mozCancelFullScreen();
-                    } else if (document.webkitExitFullscreen) {
-                        document.webkitExitFullscreen();
+                },
+                main: {
+                    label: "c",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta2 = "c";
+                        pregunta2 = false;
+                        rePlay();
                     }
                 }
-            }               
+
+            }
+        });
+    }
+    if (video.currentTime > 50.5 && pregunta3 == true) {
+        video.pause();
+        bootbox.dialog({
+            message: "a) Tirándome al suelo. <br /> b) Clavando el lateral trasero de la tabla. <br /> c) Clavando el lateral delantero de la tabla.",
+            title: "¿Como frenarías?",
+            closeButton: false,
+            animate: true,
+            buttons: {
+                success: {
+                    label: "a",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta3 = "a";
+                        pregunta3 = false;
+                        rePlay();
+                    }
+                },
+                danger: {
+                    label: "b",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta3 = "b";
+                        pregunta3 = false;
+                        rePlay();
+                    }
+                },
+                main: {
+                    label: "c",
+                    className: "btn-primary",
+                    callback: function () {
+                        respuesta3 = "c";
+                        pregunta3 = false;
+                        rePlay();
+                    }
+                }
+
+            }
+        });
+    }
+    if (video.currentTime > 65 && respuestas == true) {
+        respuestas = false;
+        video.pause();
+        $("#bodyModal").append("<label id='r1'> 1. Respuesta --> " + respuesta1 + "</label><br /><label id='r2'> 2. Respuesta --> " + respuesta2 + "</label><br /><label id='r3'> 3. Respuesta --> " + respuesta3 + "</label>");
+        if (respuesta1 == "c") {
+            $("#r1").attr("style", "background-color:#7FB77A");
+        }
+        else
+        {
+            $("#r1").attr("style", "background-color:#E87575");
+        }
+
+        if (respuesta2 == "a") {
+            $("#r2").attr("style", "background-color:#7FB77A");
+        }
+        else {
+            $("#r2").attr("style", "background-color:#E87575");
+        }
+
+        if (respuesta3 == "b") {
+            $("#r3").attr("style", "background-color:#7FB77A");
+        }
+        else {
+            $("#r3").attr("style", "background-color:#E87575");
+        }
+        $('#myModal').modal('show');
+        $("#modClose").click(function () {
+            video.play();
+        });
+        //bootbox.dialog({
+        //    message: "1. Respuesta --> " + respuesta1 + "<br /> 2. Respuesta --> " + respuesta2 + "<br /> 3. Respuesta --> " + respuesta3,
+        //    title: "Respuestas",
+        //    closeButton: false,
+        //    animate: true,
+        //    buttons: {
+        //        ok: {
+        //            label: "Ok",
+        //            className: "btn-primary",
+        //            callback: function () {
+                        
+        //                video.pause();
+        //            }
+        //        }
+        //    }
+        //});
+    }
+});
+}
+
+function rePlay() {
+setTimeout(function () {
+    video.play();
+}, 1000);
+}
+
+function volumeDown() {
+video.volume = video.volume - 0.1;
+}
+
+function volumeUp() {
+video.volume = video.volume + 0.1;
+}
+
+function fullScreen() {
+if (!document.fullscreenElement &&    // alternative standard method
+!document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
+    if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+} else {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+}
+}               
