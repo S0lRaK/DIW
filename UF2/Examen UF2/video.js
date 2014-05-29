c67bfe8fd72c31bf306e17697b0a0c70c2eec918
Carlos J. García Carmona <carlosj.gc989@gmail.com>
2014-05-29 19:29:57 +0200

    Progress Bar (Video Controls)

--- UF2/Examen UF2/video.js
+++ UF2/Examen UF2/video.js
@@ -1,4 +1,4 @@
-// Gets the all the document ready for jQuery
+// Gets all the document ready for jQuery
 $(document).ready(function()
 {
 	var vid = document.getElementById('video');
@@ -12,10 +12,26 @@ $(document).ready(function()
 	{
 		vid.pause();
 	});
-	//
+	// Slider changes video speed and its label
 	$('#sliderSpeed').change(function()
 	{
-		$('#video').get(0).playbackRate = $('#sliderSpeed').val();
-		$('#lblSpeed').text($(this).val());
+		$('#video').playbackRate = $('#sliderSpeed').val();
+		$('#lblSpeed').text('x' + $(this).val());
 	})
+	//
+	var pBar = document.getElementById('progressBar');
+	vid.addEventListener('timeupdate', function()
+	{
+		// gets % video played
+		var percent = Math.floor((100 / vid.duration) * vid.currentTime);
+		pBar.value = percent;
+		// sets and changes progress bar's attributes
+		$('#progressBar').attr(
+		{
+			aria-valuenow: percent,
+			style: 'width: ' + percent + '%;'
+		});
+		// prints % video played
+		pBar.getElementById('progressPercent').innerHTML = '%' + percent;
+	}, false)
 });
